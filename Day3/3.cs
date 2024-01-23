b5fbe5170c57dafecdfd8a8ee7dfcb5a9fec1754
VladimirReznikov <90936280+VladimirReznikov799@users.noreply.github.com>
2024-01-23 16:31:57 +0300

    solved day 3.1

---
+++ Day3/3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string lines = File.ReadAllText("../../../3.txt");
+        int sum = CalculateSum(lines);
+        Console.WriteLine($"Sum of numeric values: {sum}");
+    }
+
+    static int CalculateSum(string input)
+    {
+        string pattern = @"(\d*(?<=[^\d.\n\r].{140,142})\d+)|(\d+(?=.{140,142}[^\d.\n\r])\d*)|((?<=[^\d.\n\r])\d+)|(\d+(?=[^\d.\n\r]))";
+        Regex regex = new Regex(pattern, RegexOptions.Singleline);
+
+        int sum = regex.Matches(input)
+                       .Cast<Match>()
+                       .Select(match => int.Parse(match.Value))
+                       .Sum();
+
+        return sum;
+    }
+}
